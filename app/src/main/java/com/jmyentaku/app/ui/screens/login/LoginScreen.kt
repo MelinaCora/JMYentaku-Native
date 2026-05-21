3d3d559410a5345f7a2f3c4cadb9520f272dcbc1
MelinaCora <melinaccora97@gmail.com>
2026-05-21 14:14:25 -0300

    estados y formularios

--- app/src/main/java/com/jmyentaku/app/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/jmyentaku/app/ui/screens/login/LoginScreen.kt
@@ -2,12 +2,17 @@ package com.jmyentaku.app.ui.screens.login
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.jmyentaku.app.ui.navigation.Routes
 
@@ -16,21 +21,65 @@ fun LoginScreen(
     navController: NavController
 ) {
 
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+
         verticalArrangement = Arrangement.Center,
+
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Text(text = "Login Screen")
+        Text(text = "Login")
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text(text = "Email")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text(text = "Password")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
 
         Button(
             onClick = {
+
+                println(email)
+                println(password)
+
                 navController.navigate(Routes.Home.route)
             }
         ) {
-            Text(text = "Ir al Home")
+            Text(text = "Login")
         }
+
+        Spacer(modifier = Modifier.height(10.dp))
 
         Button(
             onClick = {
