82928b8878e190cdd33e7bb16f334b5f5f96434f
MelinaCora <melinaccora97@gmail.com>
2026-05-21 13:54:30 -0300

    navegacion por las pantallas - LoginScreen

--- app/src/main/java/com/jmyentaku/app/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/jmyentaku/app/ui/screens/login/LoginScreen.kt
@@ -1,9 +1,43 @@
 package com.jmyentaku.app.ui.screens.login
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.jmyentaku.app.ui.navigation.Routes
 
 @Composable
-fun LoginScreen() {
-    Text(text = "Login Screen")
+fun LoginScreen(
+    navController: NavController
+) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Login Screen")
+
+        Button(
+            onClick = {
+                navController.navigate(Routes.Home.route)
+            }
+        ) {
+            Text(text = "Ir al Home")
+        }
+
+        Button(
+            onClick = {
+                navController.navigate(Routes.Register.route)
+            }
+        ) {
+            Text(text = "Ir a Registro")
+        }
+    }
 }
