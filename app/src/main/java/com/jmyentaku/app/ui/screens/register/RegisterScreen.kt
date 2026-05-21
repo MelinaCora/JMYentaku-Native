ed34eb777aa07f942a9ea6cfea602ad4d25b849a
MelinaCora <melinaccora97@gmail.com>
2026-05-21 13:56:12 -0300

    navegacion por las pantallas - RegisterScreen

--- app/src/main/java/com/jmyentaku/app/ui/screens/register/RegisterScreen.kt
+++ app/src/main/java/com/jmyentaku/app/ui/screens/register/RegisterScreen.kt
@@ -1,9 +1,35 @@
 package com.jmyentaku.app.ui.screens.register
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.jmyentaku.app.ui.navigation.Routes
 
 @Composable
-fun RegisterScreen() {
-    Text(text = "Register Screen")
+fun RegisterScreen(
+    navController: NavController
+) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Register Screen")
+
+        Button(
+            onClick = {
+                navController.navigate(Routes.Login.route)
+            }
+        ) {
+            Text(text = "Volver al Login")
+        }
+    }
 }
