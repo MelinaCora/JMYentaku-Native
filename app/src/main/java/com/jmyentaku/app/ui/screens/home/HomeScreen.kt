b7a4d42ee6af0b6f1417f60d6689b32e394fa8d6
MelinaCora <melinaccora97@gmail.com>
2026-05-22 20:04:37 -0300

    boton de logout en homeScreen

--- app/src/main/java/com/jmyentaku/app/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/jmyentaku/app/ui/screens/home/HomeScreen.kt
@@ -7,16 +7,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import com.jmyentaku.app.ui.components.GeneralComponent.CustomButton
+import com.jmyentaku.app.ui.navigation.Routes
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    navController: NavController
+) {
 
     Column(
         modifier = Modifier.fillMaxSize(),
+
         verticalArrangement = Arrangement.Center,
+
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Text(text = "Home Screen")
+
+        CustomButton(
+            text = "Cerrar sesión",
+            onClick = {
+
+                FirebaseAuth
+                    .getInstance()
+                    .signOut()
+
+                navController.navigate(
+                    Routes.Login.route
+                ) {
+
+                    popUpTo(0)
+                }
+            }
+        )
     }
 }
