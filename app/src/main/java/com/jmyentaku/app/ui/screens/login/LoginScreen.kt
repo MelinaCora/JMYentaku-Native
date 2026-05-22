5f12c1e4cef6bf6b0f90da2b3909ef6764f5c5f0
MelinaCora <melinaccora97@gmail.com>
2026-05-22 10:48:04 -0300

    reemplazamos los outlinedtextfield y button por los componentes

--- app/src/main/java/com/jmyentaku/app/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/jmyentaku/app/ui/screens/login/LoginScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -15,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.jmyentaku.app.ui.navigation.Routes
+import com.jmyentaku.app.ui.components.GeneralComponent.CustomButton
+import com.jmyentaku.app.ui.components.form.CustomTextField
 
 @Composable
 fun LoginScreen(
@@ -43,50 +43,40 @@ fun LoginScreen(
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(
+        CustomTextField(
             value = email,
             onValueChange = {
                 email = it
             },
-            label = {
-                Text(text = "Email")
-            }
+            label = "Email"
         )
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        OutlinedTextField(
+        CustomTextField(
             value = password,
             onValueChange = {
                 password = it
             },
-            label = {
-                Text(text = "Password")
-            }
+            label = "Password"
         )
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Button(
+        CustomButton(
+            text = "Login",
             onClick = {
-
-                println(email)
-                println(password)
-
                 navController.navigate(Routes.Home.route)
             }
-        ) {
-            Text(text = "Login")
-        }
+        )
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        Button(
+        CustomButton(
+            text = "Registro",
             onClick = {
                 navController.navigate(Routes.Register.route)
             }
-        ) {
-            Text(text = "Ir a Registro")
-        }
+        )
     }
 }
