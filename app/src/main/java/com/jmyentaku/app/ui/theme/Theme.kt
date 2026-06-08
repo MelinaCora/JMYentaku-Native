3e796d15eaf021267299c08baf08df7d032cc200
MelinaCora <melinaccora97@gmail.com>
2026-06-08 18:26:42 -0300

    cambiamos theme por error en algunos botones

--- app/src/main/java/com/jmyentaku/app/ui/theme/Theme.kt
+++ app/src/main/java/com/jmyentaku/app/ui/theme/Theme.kt
@@ -11,53 +11,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.graphics.Color
 
-// ------------------------
-// 🎨 PALETA CELESTE MODERNA
-// ------------------------
-
-// Light
-val SkyBlue40 = Color(0xFF0288D1)
-val BlueGrey40 = Color(0xFF546E7A)
-val Aqua40 = Color(0xFF00ACC1)
-
-// Dark
-val SkyBlue80 = Color(0xFF81D4FA)
-val BlueGrey80 = Color(0xFFB0BEC5)
-val Aqua80 = Color(0xFF80DEEA)
-
-// ------------------------
-// 🌙 DARK THEME
-// ------------------------
 private val DarkColorScheme = darkColorScheme(
-    primary = JMY_SkyBlue40,
-    secondary = JMY_BlueGrey40,
-    tertiary = JMY_Aqua40
-)
-
-// ------------------------
-// ☀️ LIGHT THEME
-// ------------------------
-private val LightColorScheme = lightColorScheme(
     primary = JMY_SkyBlue80,
     secondary = JMY_BlueGrey80,
     tertiary = JMY_Aqua80
 )
 
+private val LightColorScheme = lightColorScheme(
+    primary = JMY_SkyBlue40,
+    secondary = JMY_BlueGrey40,
+    tertiary = JMY_Aqua40
+)
+
 @Composable
 fun JMYentakuTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context)
-            else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
     }
 
     MaterialTheme(
